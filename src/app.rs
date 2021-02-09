e756147798954bcfe8579cc9e7679be6c75a4d6d
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2021-02-09 13:15:27 +0100

    Add mod app.rs

---
+++ src/app.rs
@@ -0,0 +1,57 @@
+use std::path::{Path, PathBuf};
+use std::{fs, io};
+
+use structopt::StructOpt;
+
+use crate::error::Result;
+use crate::jwt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(global_setting = structopt::clap::AppSettings::ColoredHelp)]
+/// Encode and decode JSON web tokens
+pub enum Command {
+    /// Decodes a JSON web token
+    Decode {
+        /// Input file
+        input: PathBuf,
+        /// Output file
+        #[structopt(default_value = "-")]
+        output: PathBuf,
+    },
+    /// Encodes a JSON web token
+    Encode {
+        /// Input file
+        input: PathBuf,
+        /// Output file
+        #[structopt(default_value = "-")]
+        output: PathBuf,
+    },
+}
+
+fn get_read<P: AsRef<Path>>(input: P) -> Result<Box<dyn io::Read>> {
+    Ok(if input.as_ref() == Path::new("-") {
+        Box::new(io::stdin())
+    } else {
+        Box::new(fs::File::open(input)?)
+    })
+}
+
+fn get_write<P: AsRef<Path>>(output: P) -> Result<Box<dyn io::Write>> {
+    Ok(if output.as_ref() == Path::new("-") {
+        Box::new(io::stdout())
+    } else {
+        Box::new(fs::File::create(output)?)
+    })
+}
+
+pub fn main(command: Command) -> Result<()> {
+    match command {
+        Command::Decode { input, output } => {
+            jwt::decode(&mut get_read(&input)?, &mut get_write(output)?)?;
+        }
+        Command::Encode { input, output } => {
+            jwt::encode(&mut get_read(&input)?, &mut get_write(output)?)?;
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,12 @@
-use std::path::{Path, PathBuf};
-use std::{fs, io};
-
 use structopt::StructOpt;
 
+mod app;
 mod error;
 mod jwt;
 
 use crate::error::Result;
 
-#[derive(Debug, StructOpt)]
-#[structopt(global_setting = structopt::clap::AppSettings::ColoredHelp)]
-/// Encode and decode JSON web tokens
-enum Command {
-    /// Decodes a JSON web token
-    Decode {
-        /// Input file
-        input: PathBuf,
-        /// Output file
-        #[structopt(default_value = "-")]
-        output: PathBuf,
-    },
-    /// Encodes a JSON web token
-    Encode {
-        /// Input file
-        input: PathBuf,
-        /// Output file
-        #[structopt(default_value = "-")]
-        output: PathBuf,
-    },
-}
-
-fn get_read<P: AsRef<Path>>(input: P) -> Result<Box<dyn io::Read>> {
-    Ok(if input.as_ref() == Path::new("-") {
-        Box::new(io::stdin())
-    } else {
-        Box::new(fs::File::open(input)?)
-    })
-}
-
-fn get_write<P: AsRef<Path>>(output: P) -> Result<Box<dyn io::Write>> {
-    Ok(if output.as_ref() == Path::new("-") {
-        Box::new(io::stdout())
-    } else {
-        Box::new(fs::File::create(output)?)
-    })
-}
-
 fn main() -> Result<()> {
-    let command = Command::from_args();
-    match command {
-        Command::Decode { input, output } => {
-            jwt::decode(&mut get_read(&input)?, &mut get_write(output)?)?;
-        }
-        Command::Encode { input, output } => {
-            jwt::encode(&mut get_read(&input)?, &mut get_write(output)?)?;
-        }
-    }
-    Ok(())
+    let command = app::Command::from_args();
+    app::main(command)
 }
