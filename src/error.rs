0e198705ee835eba248d190803995f5ca4006056
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2021-02-10 08:26:31 +0100

    Fix error enum capitalization

--- src/error.rs
+++ src/error.rs
@@ -2,9 +2,9 @@ use derive_more::{Display, Error, From};
 
 #[derive(Debug, Display, From, Error)]
 pub enum Error {
-    IO(std::io::Error),
-    JSON(serde_json::error::Error),
-    JWT(jsonwebtoken::errors::Error),
+    Io(std::io::Error),
+    Json(serde_json::error::Error),
+    Jwt(jsonwebtoken::errors::Error),
 }
 
 #[must_use]
