6aa7c355be9fcda1e838d9168ea698b2ebe5c0b9
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2021-02-14 07:38:38 +0100

    Improve CLI file representation

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use std::io;
-use std::path::Path;
 
 use structopt::clap::crate_name;
 use structopt::StructOpt;
@@ -9,52 +8,31 @@ use crate::cli;
 use crate::error::Result;
 use crate::jwt;
 
-fn path_is_dash<P: AsRef<Path>>(path: P) -> bool {
-    path.as_ref().as_os_str() == "-"
-}
-
-fn get_read<P: AsRef<Path>>(input: P) -> Result<Box<dyn io::Read>> {
-    Ok(if path_is_dash(&input) {
-        Box::new(io::stdin())
-    } else {
-        Box::new(fs::File::open(&input)?)
+fn get_read(input: &cli::Input) -> Result<Box<dyn io::Read>> {
+    Ok(match input {
+        cli::Input::Stdin => Box::new(io::stdin()),
+        cli::Input::Path(path) => Box::new(fs::File::open(path)?),
     })
 }
 
-fn get_write<P: AsRef<Path>>(output: P) -> Result<Box<dyn io::Write>> {
-    Ok(if path_is_dash(&output) {
-        Box::new(io::stdout())
-    } else {
-        Box::new(fs::File::create(&output)?)
+fn get_write(output: &cli::Output) -> Result<Box<dyn io::Write>> {
+    Ok(match output {
+        cli::Output::Stdout => Box::new(io::stdout()),
+        cli::Output::Path(path) => Box::new(fs::File::create(path)?),
     })
 }
 
 pub fn main(args: cli::Args) -> Result<()> {
     match args {
         cli::Args::Decode { input, output } => {
-            jwt::decode(&mut get_read(&input)?, &mut get_write(output)?)?;
+            jwt::decode(&mut get_read(&input)?, &mut get_write(&output)?)?;
         }
         cli::Args::Encode { input, output } => {
-            jwt::encode(&mut get_read(&input)?, &mut get_write(output)?)?;
+            jwt::encode(&mut get_read(&input)?, &mut get_write(&output)?)?;
         }
         cli::Args::Completion { shell, output } => {
-            cli::Args::clap().gen_completions_to(crate_name!(), shell, &mut get_write(output)?);
+            cli::Args::clap().gen_completions_to(crate_name!(), shell, &mut get_write(&output)?);
         }
     }
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use std::path::Path;
-
-    use super::path_is_dash;
-
-    #[test]
-    fn test_path_is_dash() {
-        assert!(path_is_dash(Path::new("-")));
-        assert!(!path_is_dash(Path::new("--")));
-        assert!(!path_is_dash(Path::new(".")));
-        assert!(!path_is_dash(Path::new("/tmp/foo")));
-    }
 }
--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,64 @@
+use std::ffi;
+use std::fmt;
 use std::path::PathBuf;
 
 use structopt::clap::Shell;
 use structopt::StructOpt;
+
+#[inline]
+fn is_dash(value: &ffi::OsStr) -> bool {
+    value == "-"
+}
+
+#[derive(Debug)]
+pub enum Input {
+    Stdin,
+    Path(PathBuf),
+}
+
+impl From<&ffi::OsStr> for Input {
+    fn from(value: &ffi::OsStr) -> Self {
+        if is_dash(value) {
+            Self::Stdin
+        } else {
+            Self::Path(PathBuf::from(value))
+        }
+    }
+}
+
+impl fmt::Display for Input {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Stdin => write!(f, "<stdin>"),
+            Self::Path(path) => write!(f, "{}", path.display()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Output {
+    Stdout,
+    Path(PathBuf),
+}
+
+impl From<&ffi::OsStr> for Output {
+    fn from(value: &ffi::OsStr) -> Self {
+        if is_dash(value) {
+            Self::Stdout
+        } else {
+            Self::Path(PathBuf::from(value))
+        }
+    }
+}
+
+impl fmt::Display for Output {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Stdout => write!(f, "<stdout>"),
+            Self::Path(path) => write!(f, "{}", path.display()),
+        }
+    }
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(global_setting = structopt::clap::AppSettings::ColoredHelp)]
@@ -10,25 +67,27 @@ pub enum Args {
     /// Decodes a JSON web token
     Decode {
         /// Input file
-        input: PathBuf,
+        #[structopt(parse(from_os_str))]
+        input: Input,
         /// Output file
-        #[structopt(default_value = "-")]
-        output: PathBuf,
+        #[structopt(parse(from_os_str), default_value = "-")]
+        output: Output,
     },
     /// Encodes a JSON web token
     Encode {
         /// Input file
-        input: PathBuf,
+        #[structopt(parse(from_os_str))]
+        input: Input,
         /// Output file
-        #[structopt(default_value = "-")]
-        output: PathBuf,
+        #[structopt(parse(from_os_str), default_value = "-")]
+        output: Output,
     },
     /// Generates a completion file
     Completion {
         /// Shell to produce a completion file for
         shell: Shell,
         /// Output file
-        #[structopt(default_value = "-")]
-        output: PathBuf,
+        #[structopt(parse(from_os_str), default_value = "-")]
+        output: Output,
     },
 }
